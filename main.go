7b0c5481dd0c5627ff4d9dace0a9b9b8282463d1
David Skinner <skinner.david@gmail.com>
2017-02-17 09:10:45 -0600

    NewCheckbox working.

--- main.go
+++ main.go
@@ -9,11 +9,13 @@ func main() {
         name := ui.NewEntry()
         button := ui.NewButton("Greet")
         greeting := ui.NewLabel("")
+		checkbox := ui.NewCheckbox("Checkbox")
         box := ui.NewVerticalBox()
         box.Append(ui.NewLabel("Enter your name:"), false)
         box.Append(name, false)
         box.Append(button, false)
         box.Append(greeting, false)
+		box.Append(checkbox, false)
         window := ui.NewWindow("Universe", 200, 100, false)
         window.SetChild(box)
         button.OnClicked(func(*ui.Button) {
