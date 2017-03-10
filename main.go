ea624c5609f02a3194350209b77a55c7a4cc0435
David Skinner <skinner.david@gmail.com>
2017-03-10 16:46:40 -0600

    Calculator Demo.

--- main.go
+++ main.go
@@ -1,33 +1,242 @@
 package main
 
 import (
-    "github.com/andlabs/ui"
+	"fmt"
+	"image"
+	"image/color"
+	"io/ioutil"
+	"time"
+
+	"github.com/aarzilli/nucular"
+	"github.com/aarzilli/nucular/label"
+	"github.com/aarzilli/nucular/rect"
+	nstyle "github.com/aarzilli/nucular/style"
 )
 
+var whichdemo int = 3
+
+const scaling = 1.8
+
+//var theme nucular.Theme = nucular.WhiteTheme
+var theme nstyle.Theme = nstyle.DarkTheme
+
 func main() {
-    err := ui.Main(func() {
-        name := ui.NewEntry()
-        button := ui.NewButton("Greet")
-        greeting := ui.NewLabel("")
-		checkbox := ui.NewCheckbox("Checkbox")
-        box := ui.NewVerticalBox()
-        box.Append(ui.NewLabel("Enter your name:"), false)
-        box.Append(name, false)
-        box.Append(button, false)
-        box.Append(greeting, false)
-		box.Append(checkbox, false)
-        window := ui.NewWindow("Universe", 200, 100, false)
-        window.SetChild(box)
-        button.OnClicked(func(*ui.Button) {
-            greeting.SetText("Hello, " + name.Text() + "!")
-        })
-        window.OnClosing(func(*ui.Window) bool {
-            ui.Quit()
-            return true
-        })
-        window.Show()
-    })
-    if err != nil {
-        panic(err)
-    }
+	var wnd nucular.MasterWindow
+
+	switch whichdemo {
+	case 0:
+		wnd = nucular.NewMasterWindow(buttonDemo, 0)
+	case 1:
+		wnd = nucular.NewMasterWindow(basicDemo, 0)
+		go func() {
+			for {
+				time.Sleep(1 * time.Second)
+				if wnd.Closed() {
+					break
+				}
+				wnd.Changed()
+			}
+		}()
+	case 2:
+		textEditorEditor.Flags = nucular.EditSelectable
+		textEditorEditor.Buffer = []rune("prova")
+		wnd = nucular.NewMasterWindow(textEditorDemo, 0)
+	case 3:
+		var cd calcDemo
+		cd.current = &cd.a
+		wnd = nucular.NewMasterWindow(cd.calculatorDemo, 0)
+	case 5:
+		wnd = nucular.NewMasterWindow(horizontalSplit, nucular.WindowNoScrollbar)
+	case 6:
+		wnd = nucular.NewMasterWindow(widgetBoundsBug, nucular.WindowNoScrollbar)
+	case 7:
+		bs, _ := ioutil.ReadFile("overview.go")
+		multilineTextEditor.Buffer = []rune(string(bs))
+		wnd = nucular.NewMasterWindow(multilineTextEditorDemo, nucular.WindowNoScrollbar)
+	case 8:
+		pd := &panelDebug{}
+		pd.Init()
+		wnd = nucular.NewMasterWindow(pd.Update, nucular.WindowNoScrollbar)
+	case 9:
+		wnd = nucular.NewMasterWindow(nestedMenu, nucular.WindowNoScrollbar)
+	}
+	wnd.SetStyle(nstyle.FromTheme(theme, scaling))
+	wnd.Main()
+
+}
+
+func buttonDemo(w *nucular.Window) {
+	w.Row(20).Static(60, 60)
+	if w.Button(label.T("button1"), false) {
+		fmt.Printf("button pressed!\n")
+	}
+	if w.Button(label.T("button2"), false) {
+		fmt.Printf("button 2 pressed!\n")
+	}
+}
+
+type difficulty int
+
+const (
+	easy = difficulty(iota)
+	hard
+)
+
+var op difficulty = easy
+var compression int
+
+func basicDemo(w *nucular.Window) {
+	w.Row(30).Dynamic(1)
+	w.Label(time.Now().Format("15:04:05"), "RT")
+
+	w.Row(30).Static(80)
+	if w.Button(label.T("button"), false) {
+		fmt.Printf("button pressed! difficulty: %v compression: %d\n", op, compression)
+	}
+	w.Row(30).Dynamic(2)
+	if w.OptionText("easy", op == easy) {
+		op = easy
+	}
+	if w.OptionText("hard", op == hard) {
+		op = hard
+	}
+	w.Row(25).Dynamic(1)
+	w.PropertyInt("Compression:", 0, &compression, 100, 10, 1)
+}
+
+var textEditorEditor nucular.TextEditor
+
+func textEditorDemo(w *nucular.Window) {
+	w.Row(30).Dynamic(1)
+	textEditorEditor.Maxlen = 30
+	textEditorEditor.Edit(w)
+}
+
+func horizontalSplit(w *nucular.Window) {
+	w.Row(0).Dynamic(2)
+	if sw := w.GroupBegin("Left", nucular.WindowNoHScrollbar|nucular.WindowBorder); sw != nil {
+		sw.Row(18).Static(150)
+		for i := 0; i < 64; i++ {
+			sw.Label(fmt.Sprintf("%#02x", i), "LC")
+		}
+		sw.GroupEnd()
+	}
+	if sw := w.GroupBegin("Right", nucular.WindowNoHScrollbar|nucular.WindowBorder); sw != nil {
+		sw.Row(18).Static(150)
+		for i := 0; i < 64; i++ {
+			sw.Label(fmt.Sprintf("%#03o", i), "LC")
+		}
+		sw.GroupEnd()
+	}
+}
+
+func widgetBoundsBug(w *nucular.Window) {
+	w.Row(20).StaticScaled(200, 250)
+	w.Label("first", "LC")
+	w.Label("second", "LC")
+	bounds := w.WidgetBounds()
+	w.Label("third", "LC")
+	bounds2 := w.LastWidgetBounds
+	if bounds != bounds2 {
+		fmt.Printf("mismatched: %#v %#v\n", bounds, bounds2)
+	}
+}
+
+var multilineTextEditor nucular.TextEditor
+
+func multilineTextEditorDemo(w *nucular.Window) {
+	w.Row(0).Dynamic(1)
+	multilineTextEditor.Flags = nucular.EditMultiline | nucular.EditSelectable | nucular.EditClipboard
+	multilineTextEditor.Edit(w)
+}
+
+type panelDebug struct {
+	splitv          nucular.ScalableSplit
+	splith          nucular.ScalableSplit
+	showblocks      bool
+	showsingleblock bool
+	showtabs        bool
+}
+
+func (pd *panelDebug) Init() {
+	pd.splitv.MinSize = 80
+	pd.splitv.Size = 120
+	pd.splitv.Spacing = 5
+	pd.splith.MinSize = 100
+	pd.splith.Size = 300
+	pd.splith.Spacing = 5
+	pd.showtabs = true
+	pd.showsingleblock = true
+}
+
+func (pd *panelDebug) Update(w *nucular.Window) {
+	for _, k := range w.Input().Keyboard.Keys {
+		if k.Rune == 'b' {
+			pd.showsingleblock = false
+			pd.showblocks = !pd.showblocks
+		}
+		if k.Rune == 'B' {
+			pd.showsingleblock = !pd.showsingleblock
+		}
+		if k.Rune == 't' {
+			pd.showtabs = !pd.showtabs
+		}
+	}
+
+	if pd.showtabs {
+		w.Row(20).Dynamic(2)
+		w.Label("A", "LC")
+		w.Label("B", "LC")
+	}
+
+	area := w.Row(0).SpaceBegin(0)
+
+	if pd.showsingleblock {
+		w.LayoutSpacePushScaled(area)
+		bounds, out := w.Custom(nstyle.WidgetStateInactive)
+		if out != nil {
+			out.FillRect(bounds, 10, color.RGBA{0x00, 0x00, 0xff, 0xff})
+		}
+	} else {
+		leftbounds, rightbounds := pd.splitv.Vertical(w, area)
+		viewbounds, commitbounds := pd.splith.Horizontal(w, rightbounds)
+
+		w.LayoutSpacePushScaled(leftbounds)
+		pd.groupOrBlock(w, "index-files", nucular.WindowBorder)
+
+		w.LayoutSpacePushScaled(viewbounds)
+		pd.groupOrBlock(w, "index-diff", nucular.WindowBorder)
+
+		w.LayoutSpacePushScaled(commitbounds)
+		pd.groupOrBlock(w, "index-right-column", nucular.WindowNoScrollbar|nucular.WindowBorder)
+	}
+}
+
+func (pd *panelDebug) groupOrBlock(w *nucular.Window, name string, flags nucular.WindowFlags) {
+	if pd.showblocks {
+		bounds, out := w.Custom(nstyle.WidgetStateInactive)
+		if out != nil {
+			out.FillRect(bounds, 10, color.RGBA{0x00, 0x00, 0xff, 0xff})
+		}
+	} else {
+		if sw := w.GroupBegin(name, flags); sw != nil {
+			sw.GroupEnd()
+		}
+	}
+}
+
+func nestedMenu(w *nucular.Window) {
+	w.Row(20).Static(180)
+	w.Label("Test", "CC")
+	w.ContextualOpen(0, image.Point{0, 0}, w.LastWidgetBounds, func(w *nucular.Window) {
+		w.Row(20).Dynamic(1)
+		if w.MenuItem(label.TA("Submenu", "CC")) {
+			w.ContextualOpen(0, image.Point{0, 0}, rect.Rect{0, 0, 0, 0}, func(w *nucular.Window) {
+				w.Row(20).Dynamic(1)
+				if w.MenuItem(label.TA("Done", "CC")) {
+					fmt.Printf("done\n")
+				}
+			})
+		}
+	})
 }
